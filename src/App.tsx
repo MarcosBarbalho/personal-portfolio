d2360cf64ad66bb3e82fbb9215574b7467b60ca3
Marcos Barbalho <marcos.barbalho.w@gmail.com>
2026-04-15 16:22:38 -0300

    feat: add introduction

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@
  * @license Apache-2.0
  */
 
+// Node Modules
+import { useEffect } from 'react';
+
 // Components
-import { Button } from "@/components/ui/button"
+import { Hero } from '@/components/Hero';
 
 export const App = () => {
-  return <Button>Hello World</Button>
-}
+  return (
+    <main className='flex flex-col container mx-auto p-10 max-w-4xl lg:pr-10 lg:pl-0 lg:max-w-6xl'>
+      <Hero/>
+    </main>
+  );
+};
